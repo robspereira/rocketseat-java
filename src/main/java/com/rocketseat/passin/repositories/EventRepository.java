28d56bd57d15cbd540911b2f46a695a9a9a91ec6
robspereira <robertolucas335@hotmail.com>
2024-04-04 14:10:02 -0300

    first commit

---
+++ src/main/java/com/rocketseat/passin/repositories/EventRepository.java
@@ -0,0 +1,8 @@
+package com.rocketseat.passin.repositories;
+
+
+import com.rocketseat.passin.domain.event.Event;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EventRepository extends JpaRepository<Event, String> {
+}
